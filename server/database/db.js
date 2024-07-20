31b15313b8ac8de537b6526c782c53c19c27a217
mindflare30 <140968326+mindflare30@users.noreply.github.com>
2024-07-20 21:48:44 +0530

    Add files via upload

---
+++ server/database/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DBConnection = async () => {
+    const USERNAME = process.env.DB_USERNAME;
+    const PASSWORD = process.env.DB_PASSWORD;
+    try {
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
